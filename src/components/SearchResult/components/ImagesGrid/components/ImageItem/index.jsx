5df5c2037c4fe19f577c9fe9e947fc43b229ba36
Amir Attari <amir.aetari@gmail.com>
2020-11-16 12:28:36 +0330

    Add some style to the images

--- src/components/SearchResult/components/ImagesGrid/components/ImageItem/index.jsx
+++ src/components/SearchResult/components/ImagesGrid/components/ImageItem/index.jsx
@@ -4,27 +4,33 @@ import type { Element } from 'react';
 import styled from 'styled-components';
 import type { ImageItemProp } from './types.js';
 
-const Item = styled.div`
+const Container = styled.div`
   padding: 1.5em 1.3em;
   text-align: center;
 `;
 const ImageWrapper = styled.div`
   display: flex;
   border: 1px solid #dbdbdb;
-  border-radius: 2px;
+  border-radius: 5px;
   box-shadow: 0 0 6px 4px #e3e3e3;
+  transition: all 300ms ease-out;
   & img {
     border-radius: 2px;
+  }
+  &:hover {
+    transform: scale(1.01);
+    border: 1px solid #aeaeae;
+    box-shadow: 0 0 14px 2px #aaaaaa;
   }
 `;
 
 function ImageItem({ title, thumbnail }: ImageItemProp): Element<'div'> {
   return (
-    <Item>
+    <Container>
       <ImageWrapper>
         <img src={thumbnail} alt={title} width={200} height={200} />
       </ImageWrapper>
-    </Item>
+    </Container>
   );
 }
 
