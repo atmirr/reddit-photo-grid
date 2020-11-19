4aa41f8faa1911433641b6c4036fc116a80b3e8c
Amir Attari <amir.aetari@gmail.com>
2020-11-19 12:08:14 +0330

    Integrate Fetch component into the App component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import React from 'react';
+// @flow
+import React, { useState } from 'react';
 import { Provider } from 'react-redux';
-import Fetch from './Fetch';
-
+import type { Element } from 'react';
+import { debounce } from 'lodash';
+import AppBar from '@components/AppBar';
+import SearchResult from '@components/SearchResult';
 import configureStore from './store';
 
-function App() {
+function App(): Element<typeof Provider> {
   const store = configureStore();
+  const [keyword, setKeyword] = useState();
+  const debouncedSetKeyword = debounce(setKeyword, 500);
+  const handleKeywordChange = (e) => {
+    const query = e.target.value;
+    debouncedSetKeyword(query);
+  };
   return (
     <Provider store={store}>
-      <Fetch />
+      <AppBar handleKeywordChange={handleKeywordChange} />
+      <SearchResult keyword={keyword} />
     </Provider>
   );
 }
